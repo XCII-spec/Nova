49c654b8acc2692945afe91a63f8b269fda0de6a
HexVT <nohamvuemba@hotmail.com>
2026-05-16 10:37:46 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
+const express = require("express");
+const app = express();
 
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
+
+app.post("/login", (req,res)=>{
+    res.json({ ok:true });
+});
+
+app.post("/upload", (req,res)=>{
+    // ici tu gères upload fichier (multer)
+    res.json({ message:"upload ok" });
+});
+
+app.listen(3000, ()=>{
+    console.log("Server running");
+});
